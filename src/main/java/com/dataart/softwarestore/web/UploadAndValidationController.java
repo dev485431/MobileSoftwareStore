47b6a26a34022b9369cf7ba7b8ad181ed137516a
Tomasz Krzos <tomasz.krzos@ocado.com>
2016-07-18 13:28:39 +0200

    CommonsMultipartResolver support;

--- src/main/java/com/dataart/softwarestore/web/UploadAndValidationController.java
+++ src/main/java/com/dataart/softwarestore/web/UploadAndValidationController.java
@@ -1,4 +1,11 @@
 package com.dataart.softwarestore.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/uploads")
 public class UploadAndValidationController {
+
+
 }
--- src/main/java/com/dataart/softwarestore/web/config/WebMvcConfig.java
+++ src/main/java/com/dataart/softwarestore/web/config/WebMvcConfig.java
@@ -1,10 +1,12 @@
 package com.dataart.softwarestore.web.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -14,6 +16,9 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @EnableWebMvc
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Value("${uploaded.file.max.size.bytes}")
+    private Long uploadedFileMaxSizeBytes;
 
     @Bean
     public ViewResolver getViewResolver() {
@@ -26,6 +31,14 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+        resolver.setDefaultEncoding("utf-8");
+        resolver.setMaxUploadSize(uploadedFileMaxSizeBytes);
+        return resolver;
     }
 
     @Bean
