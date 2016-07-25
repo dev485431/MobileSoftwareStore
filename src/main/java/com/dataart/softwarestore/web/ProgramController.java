8cf9b880d4e4b4de3a2911cde3de3d9b294ceb9b
Tomasz Krzos <tomasz.krzos@ocado.com>
2016-07-25 15:20:23 +0200

    Working file upload; need improvement;

--- src/main/java/com/dataart/softwarestore/web/ProgramController.java
+++ src/main/java/com/dataart/softwarestore/web/ProgramController.java
@@ -14,10 +14,14 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 
@@ -27,6 +31,7 @@ public class ProgramController {
     private static final Logger LOG = Logger.getLogger(ProgramController.class);
     private static final String PROGRAM_SUBMIT_PAGE = "submit";
     private static final String PROGRAM_DETAILS_PAGE = "details";
+    private static final String UPLOAD_DIR = "/uploads/";
 
     @Autowired
     private ProgramManager programManager;
@@ -34,6 +39,8 @@ public class ProgramController {
     private CategoryManager categoryManager;
     @Autowired
     private ProgramFormValidator programFormValidator;
+    @Autowired
+    private HttpServletRequest request;
 
     @InitBinder("programForm")
     private void initProgramFormValidation(WebDataBinder binder) {
@@ -41,7 +48,7 @@ public class ProgramController {
     }
 
     @RequestMapping(value = "/submit", method = RequestMethod.GET)
-    private String getAddProgramForm(Model model, HttpSession session) {
+    public String getAddProgramForm(Model model, HttpSession session) {
         LOG.debug("Getting program submit form");
         model.addAttribute("programForm", new ProgramForm());
         session.setAttribute("allCategories", categoryManager.getAllCategories());
@@ -49,10 +56,13 @@ public class ProgramController {
     }
 
     @RequestMapping(value = "/submit", method = RequestMethod.POST)
-    public String submitAddProgramForm(Model model, @ModelAttribute("programForm") @Valid ProgramForm programForm, BindingResult result, RedirectAttributes redirect) {
+    public String submitAddProgramForm(Model model, @ModelAttribute("programForm") @Valid ProgramForm programForm, BindingResult result, RedirectAttributes redirect) throws IOException {
         if (result.hasErrors()) {
             return PROGRAM_SUBMIT_PAGE;
         }
+        // validation of uploaded file
+        uploadFile(programForm.getFile());
+
         LOG.debug("Adding new program: " + programForm.toString());
         Category category = categoryManager.getCategoryById(programForm.getCategoryId());
         Statistics statistics = new Statistics(LocalDateTime.now(), 0L);
@@ -64,14 +74,25 @@ public class ProgramController {
     }
 
     @RequestMapping(value = "/details", method = RequestMethod.GET)
-    private String getProgramDetailsPage() {
+    public String getProgramDetailsPage() {
         return PROGRAM_DETAILS_PAGE;
     }
 
     @RequestMapping(value = "/remove", method = RequestMethod.GET)
-    private String removeProgram(@RequestParam("id") Integer id) {
+    public String removeProgram(@RequestParam("id") Integer id) {
         programManager.removeProgram(id);
         return "/";
+    }
+
+    private String uploadFile(CommonsMultipartFile file) throws IOException {
+        String realPathToUploads = request.getServletContext().getRealPath(UPLOAD_DIR);
+        if (!new File(realPathToUploads).exists()) {
+            new File(realPathToUploads).mkdir();
+        }
+        LOG.debug("Uploading file to: " + realPathToUploads);
+        String newPath = realPathToUploads + file.getOriginalFilename();
+        file.transferTo(new File(newPath));
+        return newPath;
     }
 
 }
